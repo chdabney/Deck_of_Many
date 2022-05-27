927b2b677b06dca8b02cb43d7d196af19c4f8c01
Chad Dabney <chdabney@protonmail.com>
2022-05-27 15:42:53 -0400

    added json file for cards. loaded all images in image file

--- deck/views.py
+++ deck/views.py
@@ -1,11 +1,18 @@
+import json
 from django.shortcuts import render
 from django.views import View
+import json
+
+
 
 # Create your views here.
 class IndexView(View):
     template_name = "index.html"
-
+  
     def get(self, request):
-        greet = "hi"
-        context = {"greetings": greet}
+        json_data = open('./cards.json')
+        data = json.load(json_data)
+        json_data.close
+        
+        context = {"data": data}
         return render(request, self.template_name, context)
